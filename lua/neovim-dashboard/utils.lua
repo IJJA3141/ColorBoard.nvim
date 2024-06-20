b65df9db0b4271fea4be2be83687765385ef3098
IJJA3141 <alex.estella.cj@gmail.com>
2024-06-20 23:14:47 +0200

    '00.11.12 offset!!!'

--- lua/neovim-dashboard/utils.lua
+++ lua/neovim-dashboard/utils.lua
@@ -7,7 +7,7 @@ function utils.buf_is_empty(bufnr)
 end
 
 function utils.disable_move_key(bufnr)
-	local keys = { "w", "f", "b", "h", "j", "k", "l", "<Up>", "<Down>", "<Left>", "<Right>" }
+	local keys = { "w", "f", "b", "h", "j", "k", "l", "<Up>", "<Down>", "<Left>", "<Right>", "<Enter>" }
 	vim.tbl_map(function(k)
 		vim.keymap.set("n", k, "<Nop>", { buffer = bufnr })
 	end, keys)
@@ -28,7 +28,7 @@ function utils.set_move_key(bufnr, win, offset_left, offset_top, keybind)
 	local move_up = function()
 		index = index - 2
 
-		if index < 2 and index ~= -1 then
+		if index < 1 and index ~= -1 then
 			index = #keybind * 2 - 1
 		end
 
@@ -36,7 +36,7 @@ function utils.set_move_key(bufnr, win, offset_left, offset_top, keybind)
 	end
 
 	local run = function()
-		print(keybind[index].key)
+		print(keybind[index / 2].key)
 
 		vim.api.nvim_feedkeys(keybind[1].key, "n", false)
 	end
